2f16748ddef447069755b3584d3c962d134bdfc0
tanggengsheng <tanggengsheng@outlook.com>
2023-01-15 23:10:34 +0800

    1.1 BinarySearch.java

---
+++ src/basic/BinarySearch.java
@@ -0,0 +1,42 @@
+package basic;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Arrays;
+
+public class BinarySearch {
+    /**
+     * This class should not be instantiated.
+     */
+    private BinarySearch() {
+    }
+
+    public static int indexOf(int[] a, int key) {
+        int lo = 0, hi = a.length - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (key < a[mid]) hi = mid - 1;
+            else if (key > a[mid]) lo = mid + 1;
+            else return mid;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        // read the integers from a file
+        In in = new In(args[0]);
+        int[] allowlist = in.readAllInts();
+
+        // sort the array
+        Arrays.sort(allowlist);
+
+        // read integer key from standard input; print if not in allowlist
+        while (!StdIn.isEmpty()) {
+            int key = StdIn.readInt();
+            if (BinarySearch.indexOf(allowlist, key) == -1)
+                StdOut.println(key);
+        }
+    }
+}
